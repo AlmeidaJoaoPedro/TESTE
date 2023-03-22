593d675a72420b2ce14b75324686f3b2056490d9
João Pedro Almeida Miranda Silva <92739586+AlmeidaZ1802@users.noreply.github.com>
2023-03-22 16:39:27 -0300

    Update receive.py

--- receive.py
+++ receive.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import serial
+import os
 #instalar essas bibs abaixo
 
 # Define a porta serial e a velocidade de comunicação
@@ -18,25 +19,14 @@ while (a == 3):
 
 
 if str (z) == "1":
-    print("Vazamento")
+    msg = "Vazamento"
+    print(msg)
 
 if str(z) == "0":
-    print("Sem vazamento")
-        
+    msg = "Sem vazamento"
+    print(msg)
 
+os.chdir("Desktop/IC")
 
-# while True:
-#     data = ser.readline().decode("UTF-8").rstrip() # Read the data from the serial port and remove any trailing whitespace
-#     print(data) 
-
-
-#while True:
-    #Espera até receber dados serial
-    #if ser.in_waiting > 0:
-        # Lê a linha de dados recebida
-        #linha = ser.readline().decode('utf-8').rstrip()
-        # Imprime a linha recebida
-        #print(linha)
-        #img = imread(io.BytesIO(base64.b64decode(b64_string)))
-        #cv2_img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
-        #cv.imwrite("reconstructed.png", cv2_img)
+arquivo = open('response.txt', 'w+')
+arquivo.writelines(msg)
